d40558bbf2c70270dc7db31129f395ddac078a09
Anna Gabriel <yssgabriel@gmail.com>
2021-07-16 11:44:43 +0800

    Added notice feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,9 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(params.require(:article).permit(:name, :body))
     if @article.save
+      flash[:notice] = 'Article was created successfully'
       redirect_to article_path(@article)
+      byebug
     else
       render :new
     end
