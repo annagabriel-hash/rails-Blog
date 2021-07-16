73666bfc9053f0f57c1b0fdae940856056c00a21
Anna Gabriel <yssgabriel@gmail.com>
2021-07-16 15:39:53 +0800

    Added article update feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,14 +11,28 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:name, :body))
     if @article.save
       flash[:notice] = 'Article was created successfully'
       redirect_to article_path(@article)
-      byebug
     else
       render :new
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:name, :body))
+      flash[:notice] = 'Article was update successfully'
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
+  end
+
 end
