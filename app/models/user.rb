d3666dfb832fba2fcee451409b83f37cb58532a1
Anna Gabriel <yssgabriel@gmail.com>
2021-07-24 10:28:08 +0800

    Added one-to-many association between users and articles from back end

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   before_save { username.downcase! }
+
+  has_many :articles
   validates :username, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
