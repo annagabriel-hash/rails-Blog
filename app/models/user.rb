479eeb6e34b609343e7104fdf23cebe51ed67af7
Anna Gabriel <yssgabriel@gmail.com>
2021-07-24 08:38:44 +0800

    Added user test and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  before_save { username.downcase! }
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false}
 end
