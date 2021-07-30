67cbb538d4b2ae4b41b7b2d0c7e4bcf6b811134b
Anna Gabriel <yssgabriel@gmail.com>
2021-07-30 18:36:47 +0800

    Tested flash notices

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = 'Welcome #{@user.first_name}, you have successfully signed up'
+      flash[:notice] = "Welcome #{@user.first_name}, you have successfully signed up"
       redirect_to articles_path
     else
       render :new
